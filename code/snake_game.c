d1b174cb87fbddaa4e1d8ee369f2be5f5dece162
uid39869 <adam.2.orcholski-ext@continental-corporation.com>
2019-02-26 17:22:45 +0100

    added key events

--- code/snake_game.c
+++ code/snake_game.c
@@ -1,25 +1,49 @@
 #include "SDL.h"
+#include "stdio.h"
+
+void log(const char * s) {
+    printf("%s\r\n", s);
+}
 
 typedef enum
 {
-    E_MENU,
-    E_RUNNING,
-    E_PAUSED
-} game_state_t;
+    E_EMPTY,
+    E_HEAD,
+    E_TAIL,
+    E_BODY,
+    E_BUG
+} field_type_t;
 
 typedef enum
 {
-    E_START,
-//    E_OPTIONS,
-    E_EXIT
-} menu_pos_t;
+    E_KEY_UP,
+    E_KEY_DOWN,
+    E_KEY_LEFT,
+    E_KEY_RIGHT
+} key_type_t;
 
+#define FIELD_WIDTH 10
+#define FIELD_HEIGHT 10
 
 typedef struct
 {
-    game_state_t state;
-    menu_pos_t   menu_position;
+    field_type_t data[FIELD_WIDTH * FIELD_HEIGHT];
+} field_t;
 
+field_t field;
+
+field_type_t GetFieldType(int x, int y) {
+    // toto: sanity check
+    return field.data[x + y * (FIELD_HEIGHT - 1)];
+}
+
+void SetFieldType(int x, int y, field_type_t type) {
+    // toto: sanity check
+    field.data[x + y * (FIELD_HEIGHT - 1)] = type;
+}
+
+typedef struct
+{
     SDL_Window *win;
     SDL_Renderer *renderer;
 
@@ -51,27 +75,58 @@ void Snake_Init(void)
 
     SDL_SetRenderDrawColor(g_data.renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
     SDL_Rect frame;
-    frame.x = 50;
-    frame.y = 50;
-    frame.w = 500;
-    frame.h = 500;
+    frame.x = 0;
+    frame.y = 0;
+    frame.w = 50;
+    frame.h = 50;
     SDL_RenderDrawRect(g_data.renderer, &frame);
 
 }
 
+void gameplay_tick_100ms(key_type_t key_press)
+{
+}
 
 int Snake_Run(void)
 {
+    Uint32 last_time = SDL_GetTicks();
+    Uint32 current_time = 0;
+
     while (1) {
         SDL_Event e;
+        key_type_t key_press = E_KEY_RIGHT;
+
+        current_time = SDL_GetTicks();
+
         if (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
+            switch (e.type)
+            {
+            case SDL_QUIT:
                 break;
+            case SDL_KEYDOWN:
+                switch (e.key.keysym.sym)
+                {
+                case SDLK_UP:
+                    key_press = E_KEY_UP;
+                    break;
+                case SDLK_DOWN:
+                    key_press = E_KEY_DOWN;
+                    break;
+                case SDLK_LEFT:
+                    key_press = E_KEY_LEFT;
+                    break;
+                case SDLK_RIGHT:
+                    key_press = E_KEY_RIGHT;
+                    break;
+                }
             }
         }
 
         //SDL_RenderClear(g_data.renderer);
-        SDL_RenderPresent(g_data.renderer);
+        if (current_time - last_time > 100){ // 100 ms timestamp
+            SDL_RenderPresent(g_data.renderer);
+            last_time = current_time;
+        }
     }
     
     SDL_DestroyRenderer(g_data.renderer);
