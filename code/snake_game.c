d9188d6b1b6075ae52ea5aab1b9da30770667dc8
tath <tath@o2.pl>
2019-02-27 23:11:48 +0100

    added node system to hold snake co-ords and modified renderer for it

--- code/snake_game.c
+++ code/snake_game.c
@@ -7,12 +7,13 @@ void log(const char * s) {
     printf("%s\r\n", s);
 }
 
-typedef enum
-{
-    E_BODY,
-    E_HEAD,
-    E_TAIL
-} sprite_type_t;
+//typedef enum
+//{
+//    E_UP,
+//    E_DOWN,
+//    E_LEFT,
+//    E_RIGHT
+//} direction_t;
 
 typedef struct node_s node_t;
 
@@ -22,39 +23,92 @@ typedef struct node_s
     node_t * next;
     int x;
     int y;
-    sprite_type_t sprite;
+    //direction_t direction;
 } node_t;
 
-void InitSnakeNode(node_t ** start, int head_x, int head_y, int tail_x, int tail_y) {
-    // sanity check
+typedef struct snake_s
+{
+    //node_t * data;
+    node_t * head;
+    node_t * tail;
+} snake_t;
+
+void InitSnakeNode(snake_t * parent, int head_x, int head_y, int tail_x, int tail_y) {
+    // todo: sanity check
     node_t * head = (node_t*)calloc(1,sizeof(node_t));
     node_t * tail = (node_t*)calloc(1,sizeof(node_t));
 
-    head->prev = tail;
+    head->prev = tail; // [tail]<--[head]
     head->x = head_x;
     head->y = head_y;
-    head->sprite = E_HEAD;
+    //head->direction = E_RIGHT;
 
-    tail->next = head;
+    tail->next = head; // [tail]-->[head]
     tail->x = tail_x;
     tail->y = tail_y;
+    //head->direction = E_LEFT;
+
+    parent->head = head;
+    parent->tail = tail;
 }
 
 void DeinitSnakeNode(node_t ** start) {
+    // todo: dealloc all shizzles
 }
 
-void MoveHead(node_t * node, int new_x, int new_y) {
+void MoveSnake(snake_t * parent, int new_x, int new_y) {
+    // move head an
 }
 
-void MoveTail(node_t * node) {
+// create new node and change head co-ord
+void AddAndMoveHead(snake_t * parent, int new_x, int new_y) {
+    node_t * new_head = (node_t*)calloc(1, sizeof(node_t));
+    node_t * old_head = parent->head;
+
+    // [tail]<--[new_body]--[head]
+    SDL_memcpy(new_head, old_head, sizeof(node_t));
+
+    // [tail]--[old_head]--[new_head]-->0
+    new_head->next = 0;
+    // [tail]--[old_head]<--[new_head]
+    new_head->prev = old_head;
+    new_head->x = new_x;
+    new_head->y = new_y;
+    // [tail]--[old_head]-->[new_head]
+    old_head->next = new_head;
+
+    parent->head = new_head;
+}
+
+// traverse for x,y co-ord
+int CheckCollision(snake_t * parent, int x, int y) {
+    node_t * i = parent->tail;
+
+    while (1)
+    {
+        if (i) {
+            if (i->x == x && i->y == y)
+                return 1;
+        }
+        else {
+            break;
+        }
+        i = i->next;
+    }
+    return 0;
+}
+
+//
+void MoveTail(snake_t * parent) {
+
 }
 
 typedef enum
 {
     E_EMPTY,
-    //E_HEAD,
-    //E_TAIL,
-    //E_BODY,
+    E_HEAD,
+    E_TAIL,
+    E_BODY,
     E_BUG
 } field_type_t;
 
@@ -107,6 +161,7 @@ typedef struct
 {
     SDL_Window * win;
     SDL_Renderer * renderer;
+    snake_t snake;
 
     int posX;
     int posY;
@@ -136,14 +191,16 @@ void Snake_Init(void)
 
     SDL_SetRenderDrawColor(g_data.renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
 
-    field.head_x = 3;
-    field.head_y = 3;
-    field.tail_x = 2;
-    field.tail_y = 3;
+    InitSnakeNode(&g_data.snake, 3, 3, 2, 3);
 
-    // todo: fix field type? remove this for renderer
-    SetFieldType(field.head_x, field.head_y, E_BUG);
-    SetFieldType(field.tail_x, field.tail_y, E_BUG);
+    // field.head_x = 3;
+    // field.head_y = 3;
+    // field.tail_x = 2;
+    // field.tail_y = 3;
+    // 
+    // // todo: fix field type? remove this for renderer
+    // SetFieldType(field.head_x, field.head_y, E_BUG);
+    // SetFieldType(field.tail_x, field.tail_y, E_BUG);
 }
 
 void gameplay_tick_100ms(key_type_t key_press, field_t * const field)
@@ -162,14 +219,16 @@ void gameplay_tick_100ms(key_type_t key_press, field_t * const field)
             // moving
             {
                 // check direction
-                int new_x = field->head_x;
-                int new_y = field->head_y;
+                // int new_x = field->head_x;
+                // int new_y = field->head_y;
+                int new_x = g_data.snake.head->x;
+                int new_y = g_data.snake.head->y;
 
                 switch (key_press)
                 {
                 case E_KEY_UP:
                     if (E_KEY_DOWN == previous_key) new_y++;
-                    else {
+                    else { // dont update when direction is not changed
                         new_y--;
                         previous_key = key_press;
                     }
@@ -207,25 +266,31 @@ void gameplay_tick_100ms(key_type_t key_press, field_t * const field)
                     break;
                 }
 
-                // todo: dont update when direction is not changed
-                //previous_key = key_press;
-
                 // check boundaries
                 if (!IsPointValid(new_x, new_y)) {
                     game_over = 1;
                 }
 
                 // check field type
+#if 1
+                if (!game_over) {
+                    if (CheckCollision(&g_data.snake, new_x, new_y)) {
+                        game_over = 1;
+                    }
+                }
+#else
                 if (!game_over) {
                     if (E_EMPTY != GetFieldType(new_x, new_y)){
                         game_over = 1;
                     }
                 }
+#endif
 
                 // change accordingly
                 if (!game_over) {
-                    SetFieldType(field->head_x, field->head_y, E_BUG); // body
-                    SetFieldType(new_x, new_y, E_BUG); // head
+                    AddAndMoveHead(&g_data.snake, new_x, new_y);
+                    //SetFieldType(field->head_x, field->head_y, E_BUG); // body
+                    //SetFieldType(new_x, new_y, E_BUG); // head
                     field->head_x = new_x;
                     field->head_y = new_y;
                 }
@@ -256,12 +321,38 @@ void draw_field(SDL_Renderer * renderer, int i) {
     SDL_RenderFillRect(renderer, &frame);
 }
 
-void game_render(SDL_Renderer * renderer , const field_t * const field) {
-    int i;
+void draw_field2(SDL_Renderer * renderer, int x, int y) {
+    // 1 field is 50 h and 50 w
+    // x + y * (LOGICAL_FIELD_HEIGHT - 1)
 
-    for (i = 0; i < LOGICAL_FIELD_WIDTH * LOGICAL_FIELD_HEIGHT; i++) {
-        if (E_EMPTY != field->data[i])
-            draw_field(renderer, i);
+    // todo: sanity check?
+    const int border_size = 1;
+
+    SDL_Rect frame;
+    frame.x = PIXEL_FIELD_WIDTH * x + border_size;
+    frame.y = PIXEL_FIELD_HEIGHT * y + border_size;
+    frame.w = PIXEL_FIELD_WIDTH - 2 * border_size;
+    frame.h = PIXEL_FIELD_HEIGHT - 2 * border_size;
+    SDL_RenderFillRect(renderer, &frame);
+}
+
+void game_render(SDL_Renderer * renderer , const field_t * const field) {
+    // int i;
+    // 
+    // for (i = 0; i < LOGICAL_FIELD_WIDTH * LOGICAL_FIELD_HEIGHT; i++) {
+    //     if (E_EMPTY != field->data[i])
+    //         draw_field(renderer, i);
+    // }
+    node_t * i = g_data.snake.tail;
+    while (1)
+    {
+        if (i) {
+            draw_field2(renderer, i->x, i->y);
+        }
+        else {
+            break;
+        }
+        i = i->next;
     }
 }
 
